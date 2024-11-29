2462d0c0200a1fc76589bce5f0b666109277ecb7
Gdugboy <gdugboy@gmail.com>
2024-11-29 14:17:24 +0900

    .env ok

--- DBpools/jerryDBpool.js
+++ DBpools/jerryDBpool.js
@@ -1,12 +1,15 @@
+// .env 파일에서 환경 변수 로드
+require('dotenv').config();
+
 // 'mysql2/promise의 promise 모듈 불러옴 (비동기 작업)
 const mysql = require('mysql2/promise');
 
 // DB 연결 풀 생성
 const jerrydb = mysql.createPool({
-    host :'localhost',
-    user :'jerry',
-    password :'1561',
-    database : 'jerryCommunity',
+    host: process.env.DB_HOST,        // .env 파일에서 가져온 값
+    user: process.env.DB_USER,        // .env 파일에서 가져온 값
+    password: process.env.DB_PASSWORD, // .env 파일에서 가져온 값
+    database: process.env.DB_NAME     // .env 파일에서 가져온 값
 });
 
 // 연결 풀 외부 내보내기
