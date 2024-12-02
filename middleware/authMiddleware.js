8d3ba0c00996fe153dce00d5675455ef61367bc6
Gdugboy <gdugboy@gmail.com>
2024-12-02 23:22:49 +0900

    before cookie session

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@
 
 // 로그인하지 않은 경우 안 됨 (세션정보 없으면 안됨)
 function isAuthenticated(req, res, next) {
-    console.log('isAuthenticated 함수에서 값 req.session.user 값 : ',req.session.user);
+    console.log('isAuthenticated 함수에서 값 req.session.user 값(미들웨어) : ',req.session.user);
     if (req.session.user) {
       // 세션에 사용자 정보가 있으면 인증됨
       return next(); // 다음 미들웨어로 넘어감
