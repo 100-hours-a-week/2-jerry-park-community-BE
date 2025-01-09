f35ff61f64acfcac6358242ec24d7b79c002316b
Gdugboy <gdugboy@gmail.com>
2025-01-09 13:31:12 +0900

    CORP fix cross>samesite

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(helmet.contentSecurityPolicy({
     },
 }));
 // cross-origin-resource-policy 설정
-app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(helmet.crossOriginResourcePolicy({ policy: 'same-site' }));
 
 // HSTS 설정 : HTTPS 강제 적용
 // app.use(helmet.hsts({
