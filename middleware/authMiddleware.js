d6f2085a3894213d42b0a7156833c913c054a02c
Gdugboy <gdugboy@gmail.com>
2025-01-13 15:56:21 +0900

    authMiddleware 401, return fix

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,11 +4,11 @@ const isAuthenticated = (req, res, next) => {
     console.log('isAuthenticated 함수에서 값 req.session.user 값(미들웨어) : ',req.session.user);
     if (!req.session.user) {
       // 인증되지 않은 경우
-      res.status(400).json({ message: '로그인이 필요합니다. (미들웨어)' });
+      res.status(401).json({ message: '로그인이 필요합니다. (미들웨어)' });
       
     }
     // 세션에 사용자 정보가 있으면 인증됨
-    return next(); // 다음 미들웨어로 넘어감
+    next(); // 다음 미들웨어로 넘어감
 };
 
 export { 
